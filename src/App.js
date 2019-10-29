8897f1c2d12ecd4c8b70bb58236ce8b451d16d9d
Devugo <ugoezenwankwo@gmail.com>
2019-10-29 03:13:22 -0700

    finalized logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
 import Pagination from './components/Pagination';
 
 const App = () => {
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const handleChange = value => {
+        setCurrentPage(value);
+    }
+    
     return (
         <div className="container">
             <Pagination 
                 totalItems={100}
                 itemsPerPage={10}
                 paginationSpan={5}
+                currentPage={currentPage}
+                handleChange={handleChange}
             />
         </div>
     )
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,55 +1,93 @@
 import React, { useState, useCallback } from 'react';
 
 const Pagination = props => {
-    // const [totalItems, itemsPerPage, paginationSpan] = props;
 
-    let pageCount = props.totalItems/props.itemsPerPage;
+    let pageCount = Math.ceil(props.totalItems/props.itemsPerPage);
     let paginationSpan = props.paginationSpan;
 
-    const [currentPage, setCurrentPage] = useState(1);
-    const [noOfPaginationSpan, setNoOfPaginationSpan] = useState(paginationSpan);
-
-    let pageNumbers = [];
-    while(pageCount > 0){
-        pageNumbers.push(pageCount);
-        pageCount--;
-    }
-    pageNumbers.sort((a, b) => a-b);
+    let pageNumbers = []; // Array to store all the pages numbers
+    let currPaginations = []; // Array to store the visible paginations based on the pagination span
     
-    const [paginations, setPaginations] = useState(pageNumbers);
+    for(var i = 1; i<=pageCount; i++){
+        if(i <= paginationSpan){
+            currPaginations.push(i);
+        }
+        pageNumbers.push(i);
+    }
+    
+    const [currentPaginations, setCurrentPaginations] = useState(currPaginations);
 
-    const navigate = useCallback((value) => {
-        setCurrentPage(value);
-    }, []);
+    const navigate = useCallback((value, type = null) => {
+        // If statements to check when the type property is set in order to enable jumoing to pages without using the individual pages buttons
+        if(type === "jump"){
+            if(value === 1){
+                setCurrentPaginations(currPaginations);
+            }else if(value === pageCount){
+                let newArr = [];
+                let ctrl = value;
+                var j = 0;
+                while(j < paginationSpan){
+                    newArr.push(ctrl);
+                    ctrl--;
+                    j++;
+                }
+                setCurrentPaginations(newArr.sort((a,b) => a-b));
+            }else if(value > pageCount){
+                return;
+            }
+            // props.handleChange(value);
+        }
 
-    console.log(pageNumbers);
+        let currPaginationsState = currentPaginations;
+        // console.log(currPaginationsState);
+
+        if(currPaginationsState.indexOf(value) <= 0 && value > 1){
+            currPaginationsState.unshift(value-1); // Add an element in front of an array
+            currPaginationsState.pop(); // Remove the last element in an array
+            props.handleChange(value);
+            // console.log(1);
+        }else if(currPaginationsState.indexOf(value) >= currPaginationsState.length - 1 && value < pageCount){
+            currPaginationsState.shift(); // Remove the first element from the array
+            currPaginationsState.push(value+1); // Add an element to the end of an array
+            props.handleChange(value);
+            // console.log(2);
+        }else if(pageNumbers.indexOf(value) > -1){
+            
+            props.handleChange(value);
+            // console.log(3);
+        }
+        
+    }, [currentPaginations, pageCount, currPaginations]);
+
+    // console.log(pageNumbers);
+    // console.log(currentPaginations);
     
     return (
         <div className="pagination-container">
             <div className="pagination-container__tabs">
-                <div className="pagination-container__tab">
+                <div onClick={navigate.bind(this, 1, 'jump')} className="pagination-container__tab">
                     <span>>></span>
                 </div>
-                <div className="pagination-container__tab">
+                <div onClick={navigate.bind(this, props.currentPage-1, 'jump')} className="pagination-container__tab">
                     <span>></span>
                 </div>
                 {
-                    paginations.length > 0 ? 
-                        paginations.map(value => 
+                    pageNumbers.length > 0 ? 
+                        currentPaginations.map(value => 
                             <div 
                                 onClick={navigate.bind(this, value)} 
                                 key={value} 
-                                className={currentPage === value ? "pagination-container__tab active" : "pagination-container__tab"}
+                                className={props.currentPage === value ? "pagination-container__tab active" : "pagination-container__tab"}
                             >
                                 <span>{value}</span>
                             </div>
                         )
                     : null
                 }
-                <div className="pagination-container__tab">
+                <div onClick={navigate.bind(this, props.currentPage+1, 'jump')} className="pagination-container__tab">
                     <span>></span>
                 </div>
-                <div className="pagination-container__tab">
+                <div onClick={navigate.bind(this, pageCount, 'jump')} className="pagination-container__tab">
                     <span>>></span>
                 </div>
             </div>
